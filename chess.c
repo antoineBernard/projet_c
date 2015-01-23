c7b982f9a5e0fc425ee30771a2cbdf12367734ab
antoinebernard <a.bernard91@gmail.com>
2015-01-23 11:20:43 +0000

    correction de fautes et bugs

--- chess.c
+++ chess.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-
-//void affichage;
+//#define MILIEU 40
+//void affichage();
+char centrage(int millieu);
 
 main() 
 {
     //le compteur pour compter les tours : si c'est impaire c'est au blanc de jouer, sinon c'est au noir
-    int choix, compteur,i;
+    int choix, compteur,i, millieu;
     //!rappel! les tableaux en C : j'indique le nombre de case
     char t[8] [8]={
         't','c','f','q','k','f','c','t',
@@ -18,7 +19,13 @@ main()
         'T','C','F','Q','K','F','C','T'
     }; 
 
-    printf("Hello World! et bienvenue le jeu d'échec !\nTaper 1 pour commencer une partie : \n");
+    printf("Hello World! et bienvenue sur le jeu d'échec !\n");
+    printf("Plusieurs choix s'offrent à vous : (tapez entrée pour valider votre choix)\n\n");
+    
+    centrage(10);
+    printf("-->   Tapez 1 pour commencer une partie\n\n");
+    centrage(10);
+    printf("-->   Tapez 2 pour quitter le jeux\n");
     scanf("%d", &choix);
     
     
@@ -26,20 +33,30 @@ main()
     {
       case 1:
         printf("C'est parti !\n");
-        
         //affichage -> dans le futur celui-ci sera une fonction
         printf("        A         B         C         D         E         F         G         H\n");
-        printf("------------------------------------------------------------------------------------\n");
+        printf("-----------------------------------------------------------------------------------\n");
         
         for (i=0; i<=7; i++) 
           {
-          /*on créé le tableau et le rempli avec la case du tableau t (au dessus)*/
+          /*on créé le tableau et le remplit avec la case du tableau t*/
           printf("%d  |    %c    |    %c    |    %c    |    %c    |    %c    |    %c    |    %c    |    %c    |\n", i+1, t[i][0], t[i][1], t[i][2], t[i][3], t[i][4], t[i][5], t[i][6], t[i][7]);
           printf("------------------------------------------------------------------------------------\n");
           }
       break;
+      
+      case 2:
+        printf("Au revoir, et a bientot !\n");
+      break;
     }
 }    
+  
+  char centrage (int millieu)//fonction de centrage, pour rendre le programme plus joli
+  {
+    int nbEspace;
+    for(nbEspace=1 ; nbEspace <= (millieu) ; nbEspace++)
+    printf(" ");
+  }
   
 /*void affichage
 {
